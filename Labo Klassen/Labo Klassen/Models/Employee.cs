baf2708aa2f945d6d0a04c3f7a8f73370e76d867
LatthakonePathavongPXL <latthakone.pathavong@student.pxl.be>
2025-11-10 12:32:12 +0100

    Labo class oefening

---
+++ Labo Klassen/Labo Klassen/Models/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo.Models
+{
+    internal class Employee
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDate;
+        private decimal _salary;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+        public decimal Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - _birthDate.Year;
+                if (DateTime.Now.DayOfYear < _birthDate.DayOfYear)
+                    age--;
+                return age;
+            }
+        }
+        public Employee()
+        {
+        }
+        public Employee(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            _salary = _salary * (1 + percentage / 100m);
+        }
+
+        public override string ToString()
+        {
+            // Toon salaris als valuta met spatie en juiste opmaak
+            CultureInfo be = new CultureInfo("nl-BE");
+            return $"Samenvatting: {_firstName} {_lastName} - {Age} - {_salary.ToString("C", be)}";
+        }
+
+
+    }
+}
---
+++ Labo Klassen/Labo Klassen/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Labo.Models;
+
+namespace Labo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string firstName = "";
+            string lastName = "";
+            DateTime birthDate;
+            decimal salary = 0m;
+            decimal percentage = 0m;
+
+            Console.Write("Voornaam: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Achternaam: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Geboortedatum: ");
+            birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Salaris: ");
+            salary = decimal.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                Console.Write("Opslag percentage: ");
+                string input = Console.ReadLine();
+                if (decimal.TryParse(input, out percentage) && percentage >= 0 && percentage <= 10)
+                {
+                    break;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Geen geldig getal...");
+                Console.ResetColor();
+            }
+
+            Employee employee = new Employee();
+            employee.FirstName = firstName;
+            employee.LastName = lastName;
+            employee.BirthDate = birthDate;
+            employee.Salary = salary;
+            employee.IncreaseSalary(percentage);
+
+            ShowDetails(employee, percentage);
+        }
+            
+            /*Console.WriteLine();
+            Console.WriteLine(employee);
+            Console.WriteLine();
+
+            Console.Write("Voornaam: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Achternaam: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Geboortedatum: ");
+            birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Salaris: ");
+            salary = decimal.Parse(Console.ReadLine());
+
+            Employee employee2 = new Employee(firstName, lastName);
+            employee2.BirthDate = birthDate;
+            employee2.Salary = salary;
+
+            Console.WriteLine();
+            Console.WriteLine(employee);
+            Console.WriteLine(employee2);
+            Console.WriteLine();
+            Console.ReadLine();
+        }*/
+        public static void ShowDetails(Employee employee, decimal percentage)
+        {
+            Employee employee1 = new Employee();
+            Console.Clear();
+            Console.WriteLine($"Opslag percentage: {percentage}");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"Werknemer: {employee.FirstName}");
+            Console.WriteLine($"Geboortedatum: {employee.BirthDate.ToString("dddd dd MMMM yyyy")} ({employee.Age})");
+            Console.WriteLine($"Salaris: {employee.Salary}");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(employee);
+        }
+    }
+}
