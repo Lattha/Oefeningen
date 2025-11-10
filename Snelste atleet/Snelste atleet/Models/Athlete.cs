d8406b9e54847d1a6b4ce99330bc77a253428461
LatthakonePathavongPXL <latthakone.pathavong@student.pxl.be>
2025-11-10 16:45:17 +0100

    oefining blackboard snelste atleet van klasse

---
+++ Snelste atleet/Snelste atleet/Models/Athlete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snelste_atleet.Models
+{
+    internal class Athlete
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+
+        public Athlete() { }
+
+        public Athlete(string name, int time) 
+        { 
+            Name = name;
+            Time = time;
+        }
+
+        public string FormattedTime   // extra property om tijd als uu:mm:ss te tonen
+        {
+            get
+            {
+                TimeSpan timespan = TimeSpan.FromSeconds(Time);
+                return timespan.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+    }
+}
---
+++ Snelste atleet/Snelste atleet/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+using Snelste_atleet.Models;
+
+namespace Snelste_atleet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int time = 0;
+            Athlete fastest = null;
+
+            do
+            {
+                Console.Write("Naam: ");
+                name = Console.ReadLine();
+
+                if (name == "STOP")
+                    break;
+
+                while (true)
+                {
+                    Console.Write("Tijd in seconden: ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine();
+
+                    if (int.TryParse(input, out time) && time > 0)
+                    {
+                        break;
+                    }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ongeldige waarde. Geef een positief tijd in.");
+                    Console.ResetColor();
+                }
+
+                Athlete current = new Athlete(name, time);
+
+                if (fastest == null || current.Time < fastest.Time)
+                {
+                    fastest = current;
+                }
+            } while (true);
+
+            Console.WriteLine();
+            Console.Write("De snelste atleet is ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(fastest.Name);
+            Console.ResetColor();
+            Console.Write(" met een tijd van ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(fastest.FormattedTime);
+            Console.ResetColor();
+        }
+    }
+}
